0529737521c288cc642d35645b5feae62544fb61
Azhenge32 <az2960426127@gmail.com>
2019-04-01 11:55:53 +0800

    Eureka Server

--- euraka/src/main/java/com/azhen/cloud/euraka/EurakaApplication.java
+++ euraka/src/main/java/com/azhen/cloud/euraka/EurakaApplication.java
@@ -1,9 +1,12 @@
 package com.azhen.cloud.euraka;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurakaApplication {
 
     public static void main(String[] args) {
