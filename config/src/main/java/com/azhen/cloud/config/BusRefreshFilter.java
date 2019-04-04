6b1ce8a289c01460f08e7b4f9d45fc8c55bb199a
Azhenge32 <az2960426127@gmail.com>
2019-04-04 15:45:32 +0800

    push git后webhook调用配置服务器刷新配置

---
+++ config/src/main/java/com/azhen/cloud/config/BusRefreshFilter.java
@@ -0,0 +1,45 @@
+package com.azhen.cloud.config;
+
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@ServletComponentScan
+@WebFilter(urlPatterns = "/*",filterName = "busRefreshFilter")
+public class BusRefreshFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest)servletRequest;
+        HttpServletResponse httpServletResponse = (HttpServletResponse)servletResponse;
+
+        String url = new String(httpServletRequest.getRequestURI());
+
+        //只过滤/actuator/bus-refresh请求
+        if (!url.endsWith("/bus-refresh")) {
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
+        //使用HttpServletRequest包装原始请求达到修改post请求中body内容的目的
+        CustomeRequestWrapper requestWrapper = new CustomeRequestWrapper(httpServletRequest);
+
+        filterChain.doFilter(requestWrapper, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
