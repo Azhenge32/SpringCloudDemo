cca40bf06a455e269264a937c03803feb597ff53
Azhenge32 <az2960426127@gmail.com>
2019-04-12 18:39:10 +0800

    zuul前置拦截器

---
+++ api-gateway/src/main/java/com/azhen/cloud/apigateway/filter/TokenFilter.java
@@ -0,0 +1,43 @@
+package com.azhen.cloud.apigateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_DECORATION_FILTER_ORDER;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+@Component
+public class TokenFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return PRE_DECORATION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext requestContent = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContent.getRequest();
+
+        String token = request.getParameter("token");
+        if (StringUtils.isEmpty(token)) {
+            requestContent.setSendZuulResponse(false);
+            requestContent.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+        }
+        return null;
+    }
+}
